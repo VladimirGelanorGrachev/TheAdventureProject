2009b4d7884a91615f068164749fa7178f0dc4ae
Gelanor <auflegen@mail.ru>
2022-06-09 16:21:34 +0300

    Update
    Update file

---
+++ Assets/Scripts/EnemyView.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+public class EnemyView : MonoBehaviour
+{
+    [SerializeField]
+    private SpriteRenderer spriteRenderer;
+
+    [SerializeField]
+    private SpriteAnimationsConfig _spriteAnimatiionsConfig;
+    private SpriteAnimator _spriteAnimator;
+
+    public SpriteRenderer SpriteRenderer => spriteRenderer;
+
+
+    private void EnemyTrack()
+    {
+        _spriteAnimator = new SpriteAnimator(_spriteAnimatiionsConfig);
+        _spriteAnimator.StartAnimation(SpriteRenderer, Track.Run, true, 10);
+    }
+
+    public void Update()
+    {
+       EnemyTrack();
+    }
+}
--- Assets/Scripts/Root.cs
+++ Assets/Scripts/Root.cs
@@ -6,15 +6,27 @@ public class Root : MonoBehaviour
     private Camera _camera;
     [SerializeField]
     private SpriteRenderer _background;
+    [SerializeField]
+    private CharacterView _characterView;
+    [SerializeField]
+    private SpriteAnimationsConfig _spriteAnimatiionsConfig;
+    [SerializeField]
+    private EnemyView _enemyView;
+    
 
     private ParalaxManager _paralaxManager;
+    private SpriteAnimator _spriteAnimator;
     private void Start()
     {
         _paralaxManager = new ParalaxManager(_camera,_background.transform);
+        _spriteAnimator = new SpriteAnimator(_spriteAnimatiionsConfig);
+        _spriteAnimator.StartAnimation(_characterView.SpriteRenderer, Track.Walk, true, 10);        
     }
     private void Update()
     {
         _paralaxManager.Update();
+        _spriteAnimator.Update();
+        _enemyView.Update();
     }
     private void FixedUpdate()
     {
